673a937df0b4255aef4229317d8db6f120131f54
Sam Powell <sam@sampowell.dev>
2026-05-26 17:20:26 -0400

    release: prepare 0.1.5

--- kernel/mix.exs
+++ kernel/mix.exs
@@ -4,7 +4,7 @@ defmodule VilanoKernel.MixProject do
   def project do
     [
       app: :vilano_kernel,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
